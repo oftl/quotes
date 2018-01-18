97eb48a25d8aff91bf8516e9e1ac5a0f364e036b
oftl <oftl@rockandroll.at>
2018-01-18 14:02:08 +0100

    add tests

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 from webtest import TestApp
+import json
 
 from quotes.quotes import Quotes
 
@@ -8,25 +9,47 @@ class Test_Quote (unittest.TestCase):
     def test (self):
         app = TestApp (Quotes ())
 
+        # random quote
+
         res = app.get ('http://localhost:8080/quote',
             status = 200,
         )
 
-        self.assertEqual (len (res.json_body.get('collection').get('items')), 1)
+        self.assertEqual (1, len (res.json_body.get('collection').get('items')))
+        self.assertEqual (res.content_type, 'application/vnd.collection+json')
 
-        # FIXME
-        #
-        #  res = app.post ('http://localhost:8080/quotes', dict (
-        #      template = dict (
-        #          data = [
-        #              dict (name = 'author',value = 'jim'),
-        #              dict (name = 'text',  value = 'i love garfield'),
-        #          ]
-        #      ),
-        #      status = 201,
-        #  ))
-        #
-        #  self.assertEqual (len (res.json_body.get('collection').get('items')), 2)
+        # specific quote
+
+        res = app.get ('http://localhost:8080/quote/1',
+            status = 200,
+        )
+
+        self.assertEqual (1, len (res.json_body.get('collection').get('items')))
+        self.assertEqual (res.content_type, 'application/vnd.collection+json')
+
+        # new quote
+
+        res = app.post ('http://localhost:8080/quotes',
+            json.dumps (dict (
+                template = dict (
+                    data = [
+                        dict (name = 'author',value = 'jim'),
+                        dict (name = 'text',  value = 'i love garfield'),
+                    ]
+                ),
+            )),
+            status = 201,
+        )
+
+        self.assertEqual (res.status_int, 201)
+        self.assertEqual (res.content_type, 'application/vnd.collection+json')
+
+        res = app.get ('http://localhost:8080/quote/2',
+            status = 200,
+        )
+
+        self.assertEqual (1, len (res.json_body.get('collection').get('items')))
+        self.assertEqual (res.content_type, 'application/vnd.collection+json')
 
 
 if __name__ == '__main__':
